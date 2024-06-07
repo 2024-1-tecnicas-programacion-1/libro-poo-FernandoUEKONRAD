0f568a4b61fdcc881882515462d225a84a125691
FernandoUEKONRAD <josef.ubaquee@konradlorenz.edu.co>
2024-06-07 13:38:07 -0500

    Actualización repositorio Libros

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,29 @@
 package com.mycompany.libropoo;
-
 public class Libro {
-    
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int publicacion;
+    private boolean leido;
+
+    public Libro(String titulo, String autor, int publicacion, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.publicacion = publicacion;
+        this.leido = false;
+        this.genero = genero;
+    }
+    public String mostrarInformacion(){
+        return ("Titulo: "+ titulo + ","+ " sAutor: "+ autor +","+" Año de publicación: "+ publicacion +","+" Género: "+ genero);
+    }
+    public void marcarLeido(){
+       leido = true;
+    }
+    public boolean esAntiguo(){
+       int diferencia = 2024-publicacion;
+       return (diferencia>=50);
+    }
+    public boolean getLeido(){
+        return leido;
+    }
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,37 @@
 package com.mycompany.libropoo;
-
+import java.util.Scanner;
 public class Principal {
-    
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        String titulo, autor, genero;
+        int anoPublicacion;
+        System.out.println("***BIENVENIDOS A SU BIBLIOTECA VIRTUAL***");
+        System.out.println("Ingrese el titulo del libro que va a retirar");
+        titulo = sc.nextLine();
+        System.out.println("Ingrese el autor del libro");
+        autor = sc.nextLine();
+        System.out.println("Ingrese el año de publicación del libro");
+        anoPublicacion = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el genero del libro");
+        genero = sc.nextLine();
+        
+        Libro book = new Libro (titulo, autor, anoPublicacion, genero);
+        String title = book.mostrarInformacion();
+      
+        System.out.println(title);
+        book.marcarLeido();
+        boolean libroLeido = book.getLeido();
+        if (libroLeido == true)
+            System.out.println("El libro ya fue leido");
+        else
+            System.out.println("El libro aún no ha sido leido");
+        
+        boolean antiguo = book.esAntiguo();
+        if (antiguo == true)
+            System.out.println("El libro tiene más de 50 años");
+        
+        else
+            System.out.println("El libro tiene menos de 50 años");
+    }    
 }
